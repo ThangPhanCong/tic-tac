762bff35a5ceecbfb269aaba1ccc793788866176
ThangPhanCong <pcthangk59@gmail.com>
2018-11-22 11:36:20 +0700

    Updates

---
+++ precache-manifest.2f8cfd1ad602bf9d536e00ed4e59e3b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "549663deb24374fdb1cb",
+    "url": "/tic-tac/static/js/runtime~main.549663de.js"
+  },
+  {
+    "revision": "2caeb222bcc1145f4197",
+    "url": "/tic-tac/static/js/main.2caeb222.chunk.js"
+  },
+  {
+    "revision": "3426c2fdac19b90fd798",
+    "url": "/tic-tac/static/js/1.3426c2fd.chunk.js"
+  },
+  {
+    "revision": "2caeb222bcc1145f4197",
+    "url": "/tic-tac/static/css/main.f6b916aa.chunk.css"
+  },
+  {
+    "revision": "3426c2fdac19b90fd798",
+    "url": "/tic-tac/static/css/1.70d3c295.chunk.css"
+  },
+  {
+    "revision": "e4158ec34ff2386ac333941c852dd67b",
+    "url": "/tic-tac/index.html"
+  }
+];
